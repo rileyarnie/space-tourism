2d62c9c681a358fc92bfc4c884ec086977ae1333
Arnold Kibira <arnold.ril3y@gmail.com>
2021-11-28 00:54:02 +0300

    Chore - Set up the tailwind utility classes for the background images

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        mobile: "url('/static/assets/home/background-home-mobile.jpg')",
+        tablet: "url('/static/assets/home/background-home-tablet.jpg')",
+        desktop: "url('/static/assets/home/background-home-desktop.jpg')",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
