b899034d2ff4affe353d116df691a50182adbd09
Arnold Kibira <arnold.ril3y@gmail.com>
2021-11-28 18:34:23 +0300

    Chore - Add custom utility classes to fit the design schema

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,28 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      colors: {
+        "text-color": "#D0D6F9",
+      },
       backgroundImage: {
         mobile: "url('/static/assets/home/background-home-mobile.jpg')",
         tablet: "url('/static/assets/home/background-home-tablet.jpg')",
         desktop: "url('/static/assets/home/background-home-desktop.jpg')",
+      },
+      fontFamily: {
+        "barlow-condensed": ['"Barlow Condensed"', "serif"],
+        bellefair: ["Bellefair", "serif"],
+        barlow: ["Barlow", "serif"],
+      },
+      fontSize: {
+        "above-base": "0.9375rem",
+        "7.5xl": "5rem",
+      },
+      lineHeight: {
+        long: "6.25rem",
+      },
+      letterSpacing: {
+        "even-wider": "0.16875rem",
       },
     },
   },
