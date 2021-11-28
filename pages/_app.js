257e034ee5da1b9b0b72b71dad2b816e28499f34
Arnold Kibira <arnold.ril3y@gmail.com>
2021-11-28 18:35:37 +0300

    Chore - Add imports to fonts used in the design

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
-import 'tailwindcss/tailwind.css'
+import "tailwindcss/tailwind.css";
+import Navbar from "../components/Navbar";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="h-screen max-h-screen bg-mobile bg-no-repeat bg-cover lg:bg-tablet xl:bg-desktop ">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Barlow&family=Barlow+Condensed&family=Bellefair&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Navbar />
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
