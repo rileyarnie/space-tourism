73463bcaa9403ebd8fb006c78a3bf4b393027c80
Arnold Kibira <arnold.ril3y@gmail.com>
2021-11-28 18:54:11 +0300

    Fix - Update jsx element

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,14 +4,14 @@ import IconClose from "./icons/IconClose";
 
 const Navbar = () => {
   return (
-    <div className=" p-6 flex justify-between items-center">
+    <nav className=" p-6 flex justify-between items-center">
       <div className="">
         <Logo />
       </div>
       <div className="">
         <IconHamburger />
       </div>
-    </div>
+    </nav>
   );
 };
 
