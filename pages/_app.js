30ac8c0830129cdc9033f50fa0a7694283c13714
Arnold Kibira <arnold.ril3y@gmail.com>
2021-11-30 19:20:57 +0300

    Chore - Create _document file and move the imported CDNsfrom the app.js file

--- pages/_app.js
+++ pages/_app.js
@@ -5,19 +5,9 @@ import Head from "next/head";
 function MyApp({ Component, pageProps }) {
   return (
     <div>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Barlow&family=Barlow+Condensed&family=Bellefair&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      {/* <Navbar /> */}
+      <Head></Head>
+      {/* <div className="h-full bg-red-500 absolute">home</div> */}
+
       <Component {...pageProps} />
     </div>
   );
